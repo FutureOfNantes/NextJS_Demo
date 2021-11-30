376d220395bdcc2c23db944a5133693946068a1a
pgrandne <perrin.grandne@protonmail.com>
2021-11-30 23:49:24 +0100

    adding a test

--- test/did_for_dases.js
+++ test/did_for_dases.js
@@ -6,8 +6,33 @@ const DidForDases = artifacts.require("DidForDases");
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
 contract("DidForDases", function (/* accounts */) {
-  it("should assert true", async function () {
-    await DidForDases.deployed();
-    return assert.isTrue(true);
+  describe("Initial deployment", async () => {
+    it("should assert true", async function () {
+      await DidForDases.deployed();
+      return assert.isTrue(true);
+    });
+
+    it("was deployed and the initial value is 10", async () => {
+      const dfd = await DidForDases.deployed();
+      const maxEntities = await dfd.maxEntities();
+      assert.equal(maxEntities, 10, 'Initial state should be 10');
+    });
+
   });
+
+  describe("Functionality", () => {
+    it("should store the value 15", async () => {
+      // get subject
+      const dfd = await DidForDases.deployed();
+   
+      // change the subject
+      await dfd.changeNumberEntities(15, { from: accounts[0] });
+   
+      // verify we changed the subject
+      const maxEntities = await dfd.maxEntities;
+      assert.equal(maxEntities, 15, `${maxEntities} was not stored!`);
+      });
+    });
+
 });
+
